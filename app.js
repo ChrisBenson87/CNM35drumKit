2aef0f39dcd8459b6f12b7c10dbbf409f1450819
Chris Benson <ChrisBenson87@gmail.com>
2022-03-01 14:55:25 +0000

    version 1.2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const drumPart = document.getElementsByClassName("drumPart")
-const drumAudio = document.getElementById("drumAudio")
+const drumPart = document.getElementsByClassName("drumPart") //should be targeting the button class
+const drumAudio = document.getElementById("drumAudio") //should be target the audio path
 
-drumPart.addEventListener("click", () => {
-    Audio.Play();
+drumPart.addEventListener("click", () => { //should ask the button to listen to a click
+    Audio.Play(drumAudio); //should play the audio path when click
 })
