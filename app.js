bb35b42419e73e244084da6063e32bd54dd89abe
Chris Benson <ChrisBenson87@gmail.com>
2022-03-07 16:34:38 +0000

    done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const highTomDrum = document.getElementById("highTomDrum");
 const highTomDrumAudio = document.getElementById("highTomDrumAudio");
 const midTomDrum = document.getElementById("midTomDrum");
 const floorTomDrum = document.getElementById("floorTomDrum");
+const floorTomDrumAudio = document.getElementById("floorTomDrumAudio");
 const hiHatCymbal = document.getElementById("hiHatCymbal");
 const hiHatCymbalAudio = document.getElementById("hiHatCymbalAudio");
 const crashCymbal = document.getElementById("crashCymbal");
@@ -17,30 +18,78 @@ bassDrum.addEventListener("click", () =>{
     bassDrumAudio.play();
 });
 
+document.addEventListener("keypress", (event) =>{
+    if(event.key === "q" || event.key === "Q"){
+        bassDrumAudio.play();
+    }
+});
+
 snareDrum.addEventListener("click", () =>{
     snareDrumAudio.play();
+});
+
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "w" || event.key == "W"){
+        snareDrumAudio.play();
+    }
 });
 
 highTomDrum.addEventListener("click", () =>{
     highTomDrumAudio.play();
 });
 
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "e" || event.key == "E"){
+        highTomDrumAudio.play();
+    }
+});
+
 midTomDrum.addEventListener("click", () =>{
     highTomDrumAudio.play();
 });
 
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "r" || event.key == "R"){
+        highTomDrumAudio.play();
+    }
+});
+
 floorTomDrum.addEventListener("click", () =>{
-    highTomDrumAudio.play();
+    floorTomDrumAudio.play();
+});
+
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "a" || event.key == "R"){
+        floorTomDrumAudio.play();
+    }
 });
 
 hiHatCymbal.addEventListener("click", () =>{
     hiHatCymbalAudio.play();
 });
 
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "s" || event.key == "S"){
+        hiHatCymbalAudio.play();
+    }
+});
+
 crashCymbal.addEventListener("click", () =>{
     crashCymbalAudio.play();
 });
 
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "d" || event.key == "D"){
+        crashCymbalAudio.play();
+    }
+});
+
 rideCymbal.addEventListener("click", () =>{
     rideCymbalAudio.play();
+});
+
+document.addEventListener("keypress", (event) =>{
+    if(event.key == "f" || event.key == "F"){
+        rideCymbalAudio.play();
+    }
 });
